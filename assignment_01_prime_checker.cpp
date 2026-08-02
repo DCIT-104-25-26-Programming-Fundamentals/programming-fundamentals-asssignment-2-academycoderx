de7c92743d49691aef68a3b3db6141ed51d48057
academycoderx <mrdavidappiagyei@gmail.com>
2026-08-02 14:43:23 +0000

    commit

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -35,5 +35,30 @@
 // =============================================================================
 
 #include <iostream>
-using namespace std;
 
+bool isPrime(int n) {
+    if (n < 2) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int number;
+
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    if (isPrime(number)) {
+        std::cout << number << " is a prime number." << std::endl;
+    } else {
+        std::cout << number << " is NOT a prime number." << std::endl;
+    }
+
+    return 0;
+}
